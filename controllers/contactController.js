990181fa783f082d80c801ff55b8d77dc1512139
KaranjaK <karanjakelvin77@gmail.com>
2023-11-14 16:19:55 +0300

    🗃️ Setups CRUD operations for getting cotacts from the db

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,9 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel")
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all Contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 //@desc Create a contact
 //@route POST /api/contacts
