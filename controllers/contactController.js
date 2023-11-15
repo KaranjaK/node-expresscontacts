7315ba287314892988ee1288845612625cb09946
KaranjaK <karanjakelvin77@gmail.com>
2023-11-15 14:23:29 +0300

    🗃️ Sets up the Database Schema

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Contact = require("../models/contactModel")
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
@@ -7,6 +8,7 @@ const getContacts = asyncHandler(async (req, res) => {
   const contacts = await Contact.find();
   res.status(200).json(contacts);
 });
+
 //@desc Create a contact
 //@route POST /api/contacts
 //@access public
