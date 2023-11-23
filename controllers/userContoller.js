21d1799636f29dc0d198adf7a2a6e3b4a3697e57
KaranjaK <karanjakelvin77@gmail.com>
2023-11-23 17:20:03 +0300

    ✨ Define ths registration fuction and install JWT

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,9 +1,36 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register the user" });
+  const {username, email, password } = req.body
+  if(!username || !email || !password){
+    res.status(400);
+    throw new Error("All Fields are Mandatory!!");
+  }
+  const userAvailable = await User.findOne({email})
+  if (userAvailable){
+    res.status(400);
+    throw new Error("The user already exist");
+  }
+
+  //Hash Password
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({_id: user.id, email: user.email});
+  }
+  else{
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
 });
 
 //@desc Login a user
