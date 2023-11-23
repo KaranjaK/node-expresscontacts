e0ea92fa69ad7987dbece19fdbd2845e8675126d
KaranjaK <karanjakelvin77@gmail.com>
2023-11-23 07:52:49 +0300

    🚚 Corrects paths to the user's routes

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,20 +1,20 @@
 const asyncHandler = require("express-async-handler");
 //@desc Register a user
-//@route POST /api/user/register
+//@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Register the user" });
 });
 
 //@desc Login a user
-//@route POST /api/user/login
+//@route POST /api/users/login
 //@access public
 const logninUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login the user" });
 });
 
 //@desc Get a user information
-//@route GET /api/user/register
+//@route GET /api/users/current
 //@access public
 const currentUser = asyncHandler(async (req, res) => {
     res.json({message: "Curent user information"});
